31dd6354ab362d6447820dfd5af3fbbcd918ea58
karla-walker <indiablonix@gmail.com>
2020-10-27 13:23:58 -0500

    Added styles to content section

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,14 @@ const toggleTag = document.querySelector('a.toggle-nav');
 const mainTag = document.querySelector('main');
 
 // when I click the toggle tag, toggle a class of 'open' on the main tag
+// and if it's open, make the toggle class say closed
+// and if it's closed, make the toggle class say open
 toggleTag.addEventListener('click', function () {
   mainTag.classList.toggle('open');
+
+  if (mainTag.classList.contains('open')) {
+    toggleTag.innerHTML = `<img src="assets/close.svg"> Close`;
+  } else {
+    toggleTag.innerHTML = `<img src="assets/menu.svg"> Menu`;
+  }
 });
